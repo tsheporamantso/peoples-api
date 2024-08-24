eab4cf931361da2f8375f407aeae26ecee2a89c5
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-24 11:07:17 +0200

    Add dummy data,

---
+++ data.js
@@ -0,0 +1,20 @@
+const people = [
+  {
+    name: 'Dumisani',
+    id: 34,
+  },
+  {
+    name: 'Mama',
+    id: 86,
+  },
+  {
+    name: 'Lebogang',
+    id: 38,
+  },
+  {
+    name: 'Tshepo',
+    id: 41,
+  },
+];
+
+module.exports = people;
