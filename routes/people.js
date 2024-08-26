275bc5e641a0d7fc20bea5ce4b54019a7be328b8
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-26 11:40:52 +0200

    Remove dead and unused code

--- routes/people.js
+++ routes/people.js
@@ -9,11 +9,6 @@ const {
 
 const router = express.Router();
 
-// router.get('/', getPeople);
-// router.post('/', createPerson);
-// router.put('/:id', updatePerson);
-// router.delete('/:id', deletePerson);
-
 router.route('/').get(getPeople).post(createPerson);
 router.route('/:id').put(updatePerson).delete(deletePerson);
 
